f11ec2a23855ffdf71f4c941217b5eae152c4bc8
svetlio_toshkin <svetlio.toshkin@gmail.com>
2019-05-25 12:56:39 +0300

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 // Http
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
 // Routing
 import { AppRoutingModule } from './app-routing.module';
+// Core Module
+import { CoreModule } from './core/core.module';
 // Users
 import { UserModule } from './users/user.module';
+// Components
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,10 @@ import { UserModule } from './users/user.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    CoreModule,
     UserModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
