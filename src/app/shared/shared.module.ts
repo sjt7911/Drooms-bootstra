8c0bb7007a894211ff6291a3166aca30ceff13e5
svetlio_toshkin <svetlio.toshkin@gmail.com>
2019-05-26 11:28:43 +0300

    Add translate lib and add file with lang keys

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgxUiLoaderModule, NgxUiLoaderRouterModule } from 'ngx-ui-loader';
 
 @NgModule({
   imports: [
     CommonModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     // Loader - spinner
     NgxUiLoaderModule,
     NgxUiLoaderRouterModule.forRoot({ showForeground: true }),
   ],
   exports: [
     CommonModule,
+    TranslateModule,
     // Loader - spinner
     NgxUiLoaderModule,
     NgxUiLoaderRouterModule
@@ -18,3 +30,8 @@ import { NgxUiLoaderModule, NgxUiLoaderRouterModule } from 'ngx-ui-loader';
 })
 
 export class SharedModule {}
+
+// Required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
